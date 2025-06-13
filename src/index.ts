3b71617e4c4746559c4a5dd4798fc0caa5db64f8
Firdaus Banjar <agam5599@gmail.com>
2025-06-13 16:28:45 +0700

    API auth

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import router from './routes/api';
 
@@ -5,8 +6,9 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use('/api', router);
-// asd
+
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
